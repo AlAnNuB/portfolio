728071547f45a58b6d46938048e1d9decfb6965a
Alan Miranda <alannub6@gmail.com>
2025-07-19 20:09:04 -0300

    feat: add useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+type Callback<T extends any[]> = (...args: T) => void;
+
+export function useDebounce<T extends any[]>(callback: Callback<T>, delay: number): (...args: T) => void {
+    let interval: NodeJS.Timeout;
+  
+    return (...args: T) => {
+      clearTimeout(interval);
+
+      interval = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+};
